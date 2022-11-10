29f62bbc8a6fd779c5c7b3db5a10427e84527c53
Ahmed Moussa <moussa.ahmed95@gmail.com>
2022-11-10 12:36:56 +0400

    feat: project init

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+
+plugins {
+    id("maven-publish")
+    id("org.jetbrains.dokka") version "1.7.10"
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+}
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("com.android.tools.build:gradle:7.2.2")
+    }
+}
+
+version = "1.0.0-alpha"
+group = "io.iohk.atala.prism"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+//    apply(plugin = "org.gradle.maven-publish")
+
+//    publishing {
+//        repositories {
+//            maven {
+//                this.name = "GitHubPackages"
+//                this.url = uri("https://maven.pkg.github.com/input-output-hk/atala-prism-wallet-sdk-kmm/")
+//                credentials {
+//                    this.username = System.getenv("ATALA_GITHUB_ACTOR")
+//                    this.password = System.getenv("ATALA_GITHUB_TOKEN")
+//                }
+//            }
+//        }
+//    }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        verbose.set(true)
+        outputToConsole.set(true)
+    }
+}
+
+rootProject.plugins.withType(NodeJsRootPlugin::class.java) {
+    rootProject.extensions.getByType(NodeJsRootExtension::class.java).nodeVersion = "16.17.0"
+}
+
+tasks.dokkaGfmMultiModule.configure {
+    outputDirectory.set(buildDir.resolve("dokkaCustomMultiModuleOutput"))
+}
