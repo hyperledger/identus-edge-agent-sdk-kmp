23bf2982c3037bab279ded8619fd4f1d5022d830
Ahmed Moussa <moussa.ahmed95@gmail.com>
2022-11-14 23:18:19 +0400

    fix
    - Handled all kotlin linting issues
    - Fixed file/class naming as we now have Java in our gradle
    - Fixe protosLib gradle file

--- core-sdk/src/androidMain/kotlin/io/iohk/atala/prism/walletcore/httpClient.kt
+++ core-sdk/src/androidMain/kotlin/io/iohk/atala/prism/walletcore/httpClient.kt
@@ -1,7 +1,8 @@
 package io.iohk.atala.prism.walletcore
 
-import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.okhttp.OkHttp
 
 internal actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(OkHttp) {
     config(this)
--- core-sdk/src/jsMain/kotlin/io/iohk/atala/prism/walletcore/httpClient.kt
+++ core-sdk/src/jsMain/kotlin/io/iohk/atala/prism/walletcore/httpClient.kt
@@ -1,7 +1,8 @@
 package io.iohk.atala.prism.walletcore
 
-import io.ktor.client.*
-import io.ktor.client.engine.js.*
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.js.Js
 
 internal actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(Js) {
     config(this)
