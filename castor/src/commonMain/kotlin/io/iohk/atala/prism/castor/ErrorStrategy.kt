2b221d1ed5114e79801c17358ac906cca5c5e150
Javier Ribó <elribonazo@gmail.com>
2023-01-22 02:42:40 +0100

    feat(didUrlParser): add did url parser and g4 grammar (#12)
    * Update g4 file.
    * Create Error Strategy class and integrate with DIDParser
    * Add tested did url parser, listener and tests.
    * Change import ordering
    * Add missing dependency
    * Specify Throw annotation in error funs that throw InvalidDidStringError and recognitionException
    * Update castor/src/commonTest/kotlin/io/iohk/atala/prism/castor/DIDUrlParserTest.kt
    * Update castor/src/commonMain/kotlin/io/iohk/atala/prism/castor/DIDUrlParser.kt
    * Update castor/src/commonTest/kotlin/io/iohk/atala/prism/castor/DIDUrlParserTest.kt
    * Update castor/src/commonTest/kotlin/io/iohk/atala/prism/castor/DIDUrlParserTest.kt
    * linter.
    * Convert DIDUrl parser to object.
    * linter
    * linting
    * linting
    
    Co-authored-by: Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Javi <javier.ribo@iohk.io>

---
+++ castor/src/commonMain/kotlin/io/iohk/atala/prism/castor/ErrorStrategy.kt
@@ -0,0 +1,31 @@
+package io.iohk.atala.prism.castor
+
+import org.antlr.v4.kotlinruntime.DefaultErrorStrategy
+import org.antlr.v4.kotlinruntime.Parser
+import org.antlr.v4.kotlinruntime.ParserRuleContext
+import org.antlr.v4.kotlinruntime.RecognitionException
+import org.antlr.v4.kotlinruntime.Token
+
+class ErrorStrategy : DefaultErrorStrategy() {
+    @Throws(RecognitionException::class)
+    override fun recover(recognizer: Parser, e: RecognitionException) {
+        var context = recognizer.context
+        while (context != null) {
+            context.exception = e
+            context = context.readParent() as ParserRuleContext?
+        }
+
+        throw e
+    }
+
+    @Throws(InvalidDIDStringError::class)
+    override fun recoverInline(recognizer: Parser): Token {
+        var context = recognizer.context
+        while (context != null) {
+            context = context.readParent() as ParserRuleContext?
+        }
+        throw InvalidDIDStringError("Invalid Did char found at [line ${recognizer.currentToken?.line}, col ${recognizer.currentToken?.charPositionInLine}] \"${recognizer.currentToken?.text}\"")
+    }
+
+    override fun sync(recognizer: Parser) {}
+}
---
+++ castor/src/commonTest/kotlin/io/iohk/atala/prism/castor/DIDUrlParserTest.kt
@@ -0,0 +1,41 @@
+package io.iohk.atala.prism.castor
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class DIDUrlParserTest {
+
+    @Test
+    fun it_should_test_valid_Urls() {
+        val didExample1 = "did:example:123456:adsd/path?query=something#fragment"
+        val didExample2 = "did:example:123456/path?query=something&query2=something#0"
+        val didExample3 = "did:example:123456/path/jpg.pp?query=something"
+
+        val parsedDID1 = DIDUrlParser.parse(didExample1)
+        val parsedDID2 = DIDUrlParser.parse(didExample2)
+        val parsedDID3 = DIDUrlParser.parse(didExample3)
+
+        assertEquals(parsedDID1.did.schema, "did")
+        assertEquals(parsedDID1.did.method, "example")
+        assertEquals(parsedDID1.did.methodId, "123456:adsd")
+        assertContentEquals(parsedDID1.path, arrayOf("path"))
+        assertEquals(parsedDID1.parameters, mapOf("query" to "something"))
+        assertEquals(parsedDID1.fragment, "fragment")
+
+        assertEquals(parsedDID2.did.schema, "did")
+        assertEquals(parsedDID2.did.method, "example")
+        assertEquals(parsedDID2.did.methodId, "123456")
+        assertContentEquals(parsedDID2.path, arrayOf("path"))
+        assertEquals(parsedDID2.parameters, mapOf("query" to "something", "query2" to "something"))
+        assertEquals(parsedDID2.fragment, "0")
+
+        assertEquals(parsedDID3.did.schema, "did")
+        assertEquals(parsedDID3.did.method, "example")
+        assertEquals(parsedDID3.did.methodId, "123456")
+        assertContentEquals(parsedDID3.path, arrayOf("path", "jpg.pp"))
+        assertEquals(parsedDID3.parameters, mapOf("query" to "something"))
+        assertNull(parsedDID3.fragment)
+    }
+}
