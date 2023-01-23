cd2db41fba7a0693d342cb8441c6bbd5df929bb7
goncalo-frade-iohk <87179681+goncalo-frade-iohk@users.noreply.github.com>
2023-01-23 20:47:07 +0000

    feat(agent): add mediator request message (#5)
    * feat(agent): add mediator request message
    * Fixes ATL-3021
    * Update prism-agent/src/commonMain/kotlin/io/iohk/atala/prism/walletsdk/prismagent/protocols/ProtocolType.kt
    * Update prism-agent/src/commonMain/kotlin/io/iohk/atala/prism/walletsdk/prismagent/protocols/MediatorRequest.kt
    * Fix linting issues
    * Fix unresolved reference
    Co-authored-by: Cristian G <cristian.castro@iohk.io>, Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Gonçalo Frade <goncalo.frade@iohk.io>

---
+++ prism-agent/build.gradle.kts
@@ -0,0 +1,155 @@
+import org.gradle.internal.os.OperatingSystem
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackOutput.Target
+
+val currentModuleName: String = "WalletAgent"
+val os: OperatingSystem = OperatingSystem.current()
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization") version "1.7.20"
+    id("com.android.library")
+    id("org.jetbrains.dokka")
+}
+
+kotlin {
+    android {
+        publishAllLibraryVariants()
+    }
+
+    jvm {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+
+    js(IR) {
+        this.moduleName = currentModuleName
+        this.binaries.executable()
+        this.useCommonJs()
+        this.compilations["main"].packageJson {
+            this.version = rootProject.version.toString()
+        }
+        this.compilations["test"].packageJson {
+            this.version = rootProject.version.toString()
+        }
+        browser {
+            this.webpackTask {
+                this.output.library = currentModuleName
+                this.output.libraryTarget = Target.VAR
+            }
+            this.commonWebpackConfig {
+                this.cssSupport {
+                    this.enabled = true
+                }
+            }
+            this.testTask {
+                this.useKarma {
+                    this.useChromeHeadless()
+                }
+            }
+        }
+        nodejs {
+            this.testTask {
+                this.useKarma {
+                    this.useChromeHeadless()
+                }
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("io.iohk.atala.prism:uuid:1.0.0-alpha")
+                implementation(project(":core-sdk"))
+                implementation(project(":domain"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val jsMain by getting
+        val jsTest by getting
+
+        all {
+            languageSettings.optIn("kotlin.RequiresOptIn")
+        }
+    }
+}
+
+android {
+    compileSdk = 32
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    /**
+     * Because Software Components will not be created automatically for Maven publishing from
+     * Android Gradle Plugin 8.0. To opt-in to the future behavior, set the Gradle property android.
+     * disableAutomaticComponentCreation=true in the `gradle.properties` file or use the new
+     * publishing DSL.
+     */
+    publishing {
+        multipleVariants {
+            withSourcesJar()
+            withJavadocJar()
+            allVariants()
+        }
+    }
+}
+
+// Dokka implementation
+tasks.withType<DokkaTask> {
+    moduleName.set(project.name)
+    moduleVersion.set(rootProject.version.toString())
+    description = """
+        This is a Kotlin Multiplatform Wallet-Core-SDK Library
+    """.trimIndent()
+    dokkaSourceSets {
+        // TODO: Figure out how to include files to the documentations
+        named("commonMain") {
+            includes.from("Module.md", "docs/Module.md")
+        }
+    }
+}
+
+// afterEvaluate {
+//    tasks.withType<AbstractTestTask> {
+//        testLogging {
+//            events("passed", "skipped", "failed", "standard_out", "standard_error")
+//            showExceptions = true
+//            showStackTraces = true
+//        }
+//    }
+// }
