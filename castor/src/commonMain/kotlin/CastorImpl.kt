df39ea34345c0163be65c5fbd4896b86a73d6fc9
Javier Ribó <elribonazo@gmail.com>
2023-01-25 18:03:27 +0100

    feat(castor): Add peerDID resolver + tests. (#14)
    * Rename DIDResolverDomain to DIDResolver
    * Move DIDParser functionality to its own folder.
    * Create PeerDIDResolver implementing DIDResolver interface
    * Adding Castor Implementation with PeerDIDResolver as default resolver and implement the resolveDID fun that gets a DID of any of the supported types and returns a DIDDocument.
    * Improve code.
    * Make DIDResolver resolve asyncronous + add jsMain implementation of Castor with promises.
    * Add testing for commonMain resolveDID fun.
    * Small Code improvement
    
    Co-authored-by: Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Javi <javier.ribo@iohk.io>

---
+++ castor/src/commonMain/kotlin/CastorImpl.kt
@@ -0,0 +1,46 @@
+package io.iohk.atala.prism.castor
+
+import io.iohk.atala.prism.domain.buildingBlocks.Castor
+import io.iohk.atala.prism.domain.models.CastorError
+import io.iohk.atala.prism.domain.models.DID
+import io.iohk.atala.prism.domain.models.DIDDocument
+import io.iohk.atala.prism.domain.models.DIDResolver
+import io.iohk.atala.prism.domain.models.KeyPair
+import io.iohk.atala.prism.domain.models.PublicKey
+
+open class CastorImpl : Castor {
+    var resolvers: Array<DIDResolver> = arrayOf(
+        PeerDIDResolver()
+    )
+
+    override fun parseDID(did: String): DID {
+        return DIDParser.parse(did)
+    }
+
+    override fun createPrismDID(masterPublicKey: PublicKey, services: Array<DIDDocument.Service>?): DID {
+        TODO("Not yet implemented")
+    }
+
+    override fun createPeerDID(
+        keyAgreementKeyPair: KeyPair,
+        authenticationKeyPair: KeyPair,
+        services: Array<DIDDocument.Service>
+    ): DID {
+        TODO("Not yet implemented")
+    }
+
+    @Throws(CastorError.NotPossibleToResolveDID::class)
+    override suspend fun resolveDID(didString: String): DIDDocument {
+        val did = DIDParser.parse(didString)
+        val resolver = resolvers.find { it.method == did.method } ?: throw CastorError.NotPossibleToResolveDID()
+        return resolver.resolve(didString)
+    }
+
+    override suspend fun verifySignature(did: DID, challenge: ByteArray, signature: ByteArray): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getEcnumbasis(did: DID, keyPair: KeyPair): String {
+        TODO("Not yet implemented")
+    }
+}
--- domain/src/commonMain/kotlin/io.iohk.atala.prism.domain/models/DIDResolverDomain.kt
+++
@@ -1,7 +0,0 @@
-package io.iohk.atala.prism.domain.models
-
-interface DIDResolverDomain {
-    val method: String
-
-    fun resolve(did: DID): DIDDocument
-}
