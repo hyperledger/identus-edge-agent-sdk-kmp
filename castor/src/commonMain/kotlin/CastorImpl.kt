485b557ad924eba152f98df74a918f2f1ee8deea
Javier Ribó <elribonazo@gmail.com>
2023-01-26 14:26:48 +0100

    feat(castor): Add peerDID Create method + tests. (#15)
    * Change publicKey and make curve KeyCurve for standarisation and sync with PrivateKey
    * Adding DIDPeer creation in Castor with couple tests.
    * Remove getEcnumbasis method from Castor interface, this is available already in didpeer package and not required here.
    * Add throw annotation to createPeerDID
    
    Co-authored-by: Goncalo-FradeIOHK <goncalo.frade@iohk.io>, Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Javi <javier.ribo@iohk.io>

--- castor/src/commonMain/kotlin/CastorImpl.kt
+++ castor/src/commonMain/kotlin/CastorImpl.kt
@@ -5,11 +5,22 @@ import io.iohk.atala.prism.domain.models.CastorError
 import io.iohk.atala.prism.domain.models.DID
 import io.iohk.atala.prism.domain.models.DIDDocument
 import io.iohk.atala.prism.domain.models.DIDResolver
+import io.iohk.atala.prism.domain.models.KeyCurve
 import io.iohk.atala.prism.domain.models.KeyPair
 import io.iohk.atala.prism.domain.models.PublicKey
+import io.iohk.atala.prism.mercury.didpeer.DIDCommServicePeerDID
+import io.iohk.atala.prism.mercury.didpeer.VerificationMaterialAgreement
+import io.iohk.atala.prism.mercury.didpeer.VerificationMaterialAuthentication
+import io.iohk.atala.prism.mercury.didpeer.VerificationMaterialFormatPeerDID
+import io.iohk.atala.prism.mercury.didpeer.VerificationMethodTypeAgreement
+import io.iohk.atala.prism.mercury.didpeer.VerificationMethodTypeAuthentication
+import io.iohk.atala.prism.mercury.didpeer.core.toJsonElement
+import io.iohk.atala.prism.mercury.didpeer.createPeerDIDNumalgo2
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 open class CastorImpl : Castor {
-    var resolvers: Array<DIDResolver> = arrayOf(
+    private var resolvers: Array<DIDResolver> = arrayOf(
         PeerDIDResolver()
     )
 
@@ -21,26 +32,71 @@ open class CastorImpl : Castor {
         TODO("Not yet implemented")
     }
 
+    @Throws(CastorError.InvalidKeyError::class)
     override fun createPeerDID(
-        keyAgreementKeyPair: KeyPair,
-        authenticationKeyPair: KeyPair,
+        keyPairs: Array<KeyPair>,
         services: Array<DIDDocument.Service>
     ): DID {
-        TODO("Not yet implemented")
+        var encryptionKeys: MutableList<VerificationMaterialAgreement> = mutableListOf()
+        var signingKeys: MutableList<VerificationMaterialAuthentication> = mutableListOf()
+
+        keyPairs.forEach {
+            when (it.curve) {
+                KeyCurve.ED25519 -> {
+                    encryptionKeys.add(
+                        VerificationMaterialAgreement(
+                            format = VerificationMaterialFormatPeerDID.MULTIBASE,
+                            value = it.publicKey.value,
+                            type = VerificationMethodTypeAgreement.X25519_KEY_AGREEMENT_KEY_2020
+                        )
+                    )
+                }
+                KeyCurve.X25519 -> {
+                    signingKeys.add(
+                        VerificationMaterialAuthentication(
+                            format = VerificationMaterialFormatPeerDID.MULTIBASE,
+                            value = it.publicKey.value,
+                            type = VerificationMethodTypeAuthentication.ED25519_VERIFICATION_KEY_2020
+                        )
+                    )
+                }
+                else -> {
+                    throw CastorError.InvalidKeyError()
+                }
+            }
+        }
+
+        if (signingKeys.isEmpty() || encryptionKeys.isEmpty()) {
+            throw CastorError.InvalidKeyError()
+        }
+
+        val peerDID = createPeerDIDNumalgo2(
+            encryptionKeys = encryptionKeys,
+            signingKeys = signingKeys,
+            service = services.map {
+                Json.encodeToString(
+                    DIDCommServicePeerDID(
+                        id = it.id,
+                        type = it.type[0],
+                        serviceEndpoint = it.serviceEndpoint.uri,
+                        routingKeys = listOf(),
+                        accept = it.serviceEndpoint.accept?.asList() ?: listOf()
+                    ).toDict().toJsonElement()
+                )
+            }.first()
+        )
+
+        return DIDParser.parse(peerDID)
     }
 
     @Throws(CastorError.NotPossibleToResolveDID::class)
-    override suspend fun resolveDID(didString: String): DIDDocument {
-        val did = DIDParser.parse(didString)
-        val resolver = resolvers.find { it.method == did.method } ?: throw CastorError.NotPossibleToResolveDID()
-        return resolver.resolve(didString)
+    override suspend fun resolveDID(did: String): DIDDocument {
+        val parsedDID = DIDParser.parse(did)
+        val resolver = resolvers.find { it.method == parsedDID.method } ?: throw CastorError.NotPossibleToResolveDID()
+        return resolver.resolve(did)
     }
 
     override suspend fun verifySignature(did: DID, challenge: ByteArray, signature: ByteArray): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun getEcnumbasis(did: DID, keyPair: KeyPair): String {
         TODO("Not yet implemented")
     }
 }
--- castor/src/commonTest/kotlin/io/iohk/atala/prism/castor/DIDResolverTest.kt
+++ castor/src/commonTest/kotlin/io/iohk/atala/prism/castor/DIDResolverTest.kt
@@ -3,6 +3,7 @@ package io.iohk.atala.prism.castor
 import io.iohk.atala.prism.domain.models.DIDDocument
 import io.iohk.atala.prism.mercury.didpeer.VerificationMethodTypeAgreement
 import io.iohk.atala.prism.mercury.didpeer.VerificationMethodTypeAuthentication
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
@@ -10,12 +11,13 @@ import kotlin.test.assertEquals
 
 class DIDResolverTest {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun it_should_resolve_valid_dids() = runTest {
         val didExample =
             "did:peer:2.Ez6LSci5EK4Ezue5QA72ZX71QUbXY2xr5ygRw7wM1WJigTNnd.Vz6MkqgCXHEGr2wJZANPZGC8WFmeVuS3abAD9uvh7mTXygCFv.SeyJ0IjoiZG0iLCJzIjoibG9jYWxob3N0OjgwODIiLCJyIjpbXSwiYSI6WyJkaWRjb21tL3YyIl19"
         val castor = CastorImpl()
-        var response = castor.resolveDID(didExample)
+        val response = castor.resolveDID(didExample)
 
         assertEquals(response.id.toString(), didExample)
 
