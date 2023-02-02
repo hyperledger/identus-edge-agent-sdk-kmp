e1e73831c051fbda2927c5a3a3da44eebea6f20c
Ribó <elribonazo@gmail.com>
2023-02-02 13:17:58 +0100

    Improvement in PrismDIDMethod data class.

--- castor/src/commonMain/kotlin/io/iohk/atala/prism/castor/DID/PrismDID/PrismDIDMethodId.kt
+++ castor/src/commonMain/kotlin/io/iohk/atala/prism/castor/DID/PrismDID/PrismDIDMethodId.kt
@@ -2,29 +2,22 @@ package io.iohk.atala.prism.castor
 
 import io.iohk.atala.prism.domain.models.CastorError
 
-class PrismDIDMethodId {
-
-    private var value: String
-    val sections: List<String>
-        get() = value.split(":")
+data class PrismDIDMethodId(private val value: String) {
+    private val sections: List<String>
+        get() = value.split(":").map { it }
 
     override fun toString(): String {
-        return this.value
+        return value
     }
 
-    constructor(sections: List<String>) {
+    constructor(sections: List<String>) : this(sections.joinToString(":")) {
         val sectionRegex = Regex("^[A-Za-z0-9_-]+$")
         if (!sections.all { sectionRegex.matches(it) }) {
             throw CastorError.MethodIdIsDoesNotSatisfyRegex()
         }
-        this.value = sections.joinToString(":")
-    }
-
-    constructor(string: String) {
         val methodSpecificIdRegex = Regex("^([A-Za-z0-9_-]*:)*[A-Za-z0-9_-]+$")
-        if (!methodSpecificIdRegex.matches(string)) {
+        if (!methodSpecificIdRegex.matches(value)) {
             throw CastorError.MethodIdIsDoesNotSatisfyRegex()
         }
-        this.value = string
     }
 }
