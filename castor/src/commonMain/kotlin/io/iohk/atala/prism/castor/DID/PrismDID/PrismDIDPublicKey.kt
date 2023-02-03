d9b38e254a3869343c4f511cbac6bbdb2b844a76
Ribó <elribonazo@gmail.com>
2023-02-03 14:23:24 +0100

    Improve code to avoid linting issue.

--- castor/src/commonMain/kotlin/io/iohk/atala/prism/castor/DID/PrismDID/PrismDIDPublicKey.kt
+++ castor/src/commonMain/kotlin/io/iohk/atala/prism/castor/DID/PrismDID/PrismDIDPublicKey.kt
@@ -17,35 +17,7 @@ class PrismDIDPublicKey {
         CAPABILITY_DELEGATION_KEY("capabilityDelegationKey"),
         CAPABILITY_INVOCATION_KEY("capabilityInvocationKey"),
         KEY_AGREEMENT_KEY("keyAgreementKey"),
-        UNKNOWN_KEY("unknownKey");
-
-        val defaultId: String get() = id(0)
-
-        fun toProto(): KeyUsage {
-            return when (this) {
-                MASTER_KEY -> KeyUsage.MASTER_KEY
-                ISSUING_KEY -> KeyUsage.ISSUING_KEY
-                AUTHENTICATION_KEY -> KeyUsage.AUTHENTICATION_KEY
-                REVOCATION_KEY -> KeyUsage.REVOCATION_KEY
-                CAPABILITY_DELEGATION_KEY -> KeyUsage.CAPABILITY_DELEGATION_KEY
-                CAPABILITY_INVOCATION_KEY -> KeyUsage.CAPABILITY_INVOCATION_KEY
-                KEY_AGREEMENT_KEY -> KeyUsage.KEY_AGREEMENT_KEY
-                UNKNOWN_KEY -> KeyUsage.UNKNOWN_KEY
-            }
-        }
-
-        fun id(index: Int): String {
-            return when (this) {
-                MASTER_KEY -> "master$index"
-                ISSUING_KEY -> "issuing$index"
-                AUTHENTICATION_KEY -> "authentication$index"
-                REVOCATION_KEY -> "revocation$index"
-                CAPABILITY_DELEGATION_KEY -> "capabilityDelegation$index"
-                CAPABILITY_INVOCATION_KEY -> "capabilityInvocation$index"
-                KEY_AGREEMENT_KEY -> "keyAgreement$index"
-                UNKNOWN_KEY -> "unknown$index"
-            }
-        }
+        UNKNOWN_KEY("unknownKey")
     }
 
     private val apollo: Apollo
@@ -105,4 +77,30 @@ fun CompressedPublicKey.toProto(): CompressedECKeyData {
         curve = uncompressed.curve.curve.value,
         data = ByteArr(value)
     )
+}
+
+fun PrismDIDPublicKey.Usage.id(index:Int): String {
+    return when (this) {
+        PrismDIDPublicKey.Usage.MASTER_KEY -> "master$index"
+        PrismDIDPublicKey.Usage.ISSUING_KEY -> "issuing$index"
+        PrismDIDPublicKey.Usage.AUTHENTICATION_KEY -> "authentication$index"
+        PrismDIDPublicKey.Usage.REVOCATION_KEY -> "revocation$index"
+        PrismDIDPublicKey.Usage.CAPABILITY_DELEGATION_KEY -> "capabilityDelegation$index"
+        PrismDIDPublicKey.Usage.CAPABILITY_INVOCATION_KEY -> "capabilityInvocation$index"
+        PrismDIDPublicKey.Usage.KEY_AGREEMENT_KEY -> "keyAgreement$index"
+        PrismDIDPublicKey.Usage.UNKNOWN_KEY -> "unknown$index"
+    }
+}
+
+fun PrismDIDPublicKey.Usage.toProto(): KeyUsage {
+    return when (this) {
+        PrismDIDPublicKey.Usage.MASTER_KEY -> KeyUsage.MASTER_KEY
+        PrismDIDPublicKey.Usage.ISSUING_KEY -> KeyUsage.ISSUING_KEY
+        PrismDIDPublicKey.Usage.AUTHENTICATION_KEY -> KeyUsage.AUTHENTICATION_KEY
+        PrismDIDPublicKey.Usage.REVOCATION_KEY -> KeyUsage.REVOCATION_KEY
+        PrismDIDPublicKey.Usage.CAPABILITY_DELEGATION_KEY -> KeyUsage.CAPABILITY_DELEGATION_KEY
+        PrismDIDPublicKey.Usage.CAPABILITY_INVOCATION_KEY -> KeyUsage.CAPABILITY_INVOCATION_KEY
+        PrismDIDPublicKey.Usage.KEY_AGREEMENT_KEY -> KeyUsage.KEY_AGREEMENT_KEY
+        PrismDIDPublicKey.Usage.UNKNOWN_KEY -> KeyUsage.UNKNOWN_KEY
+    }
 }
