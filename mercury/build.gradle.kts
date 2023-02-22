21449a14f26fa92a0186241a69e20c80aa3ca4aa
goncalo-frade-iohk <87179681+goncalo-frade-iohk@users.noreply.github.com>
2023-02-22 18:24:03 +0000

    feat(mercury): add default secrets resolver (#34)
    * feat(mercury): add default secrets resolver
    
    This also adds changes on pluto with a comment on PrismAgent to explain the change.
    
    * Update domain/src/commonMain/kotlin/io.iohk.atala.prism.walletsdk.domain/models/Secret.kt
    
    * Update mercury/src/allButJSMain/kotlin/io.iohk.atala.prism.walletsdk.mercury/MercuryImpl.kt
    
    * Update mercury/src/allButJSMain/kotlin/io.iohk.atala.prism.walletsdk.mercury/MercuryImpl.kt
    
    * Fix lint issues and failing tests
    
    ---------
    
    Co-authored-by: Cristian G <113917899+cristianIOHK@users.noreply.github.com>
    Co-authored-by: Cristian G <cristian.castro@iohk.io>

--- mercury/build.gradle.kts
+++ mercury/build.gradle.kts
@@ -66,6 +66,8 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation(project(":domain"))
+                implementation("io.iohk.atala.prism:apollo:1.0.0-alpha")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
             }
@@ -75,18 +77,29 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        val jvmMain by getting
+        val allButJSMain by creating {
+            this.dependsOn(commonMain)
+        }
+        val allButJSTest by creating {
+            this.dependsOn(commonTest)
+        }
+        val jvmMain by getting {
+            this.dependsOn(allButJSMain)
+        }
         val jvmTest by getting {
+            this.dependsOn(allButJSTest)
             dependencies {
                 implementation("junit:junit:4.13.2")
             }
         }
         val androidMain by getting {
+            this.dependsOn(allButJSMain)
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
             }
         }
         val androidTest by getting {
+            this.dependsOn(allButJSTest)
             dependencies {
                 implementation("junit:junit:4.13.2")
             }
--- prism-agent/src/allButJSTest/kotlin/io.iohk.atala.prism.walletsdk.prismagent/CastorMock.kt
+++ prism-agent/src/allButJSTest/kotlin/io.iohk.atala.prism.walletsdk.prismagent/CastorMock.kt
@@ -12,7 +12,7 @@ class CastorMock : Castor {
     var createPeerDIDReturn: DID? = DID(
         "did",
         "prism",
-        "b6c0c33d701ac1b9a262a14454d1bbde3d127d697a76950963c5fd930605:Cj8KPRI7CgdtYXN0ZXIwEAFKLgoJc2VmsxEiECSTjyV7sUfCr_ArpN9rvCwR9fRMAhcsr_S7ZRiJk4p5k"
+        "b6c0c33d701ac1b9a262a14454d1bbde3d127d697a76950963c5fd930605:Cj8KPRI7CgdtYXN0ZXIwEAFKLgoJc2VmsxEiECSTjyV7sUfCr_ArpN9rvCwR9fRMAhcsr_S7ZRiJk4p5k",
     )
     var resolveDIDReturn: DIDDocument? = null
     var verifySignatureReturn: Boolean = false
@@ -23,19 +23,36 @@ class CastorMock : Castor {
 
     override fun createPrismDID(
         masterPublicKey: PublicKey,
-        services: Array<DIDDocument.Service>?
+        services: Array<DIDDocument.Service>?,
     ): DID {
         return createPrismDIDReturn ?: throw Exception("createPrismDID() not implemented in mock")
     }
 
     override fun createPeerDID(
         keyPairs: Array<KeyPair>,
-        services: Array<DIDDocument.Service>
+        services: Array<DIDDocument.Service>,
     ): DID {
         return createPeerDIDReturn ?: throw Exception("createPeerDID() not implemented in mock")
     }
 
     override suspend fun resolveDID(did: String): DIDDocument {
+        val coreProperty = DIDDocument.Service(
+            id = "DIDCommV2",
+            type = arrayOf("DIDCommMessaging"),
+            serviceEndpoint = DIDDocument.ServiceEndpoint(
+                uri = "localhost:8082",
+                accept = arrayOf("DIDCommMessaging"),
+                routingKeys = arrayOf(),
+            ),
+        )
+        resolveDIDReturn = DIDDocument(
+            DID(
+                "did",
+                "prism",
+                "b6c0c33d701ac1b9a262a14454d1bbde3d127d697a76950963c5fd930605:Cj8KPRI7CgdtYXN0ZXIwEAFKLgoJc2VmsxEiECSTjyV7sUfCr_ArpN9rvCwR9fRMAhcsr_S7ZRiJk4p5k",
+            ),
+            arrayOf(coreProperty),
+        )
         return resolveDIDReturn ?: throw Exception("resolveDID() not implemented in mock")
     }
 
