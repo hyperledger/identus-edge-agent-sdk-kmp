5a0ef2f0786f2837661fd2f30457439aa04c8d4e
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-03-17 16:41:51 +0200

    chore: added libs (#44)
    Signed-off-by: Ahmed Moussa <ahmed.moussa@iohk.io>

--- apollo/build.gradle.kts
+++ apollo/build.gradle.kts
@@ -66,6 +66,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
                 implementation("io.iohk.atala.prism:base-asymmetric-encryption:$apolloVersion")
                 implementation("io.iohk.atala.prism:ecdsa:$apolloVersion")
+                implementation("com.nimbusds:nimbus-jose-jwt:9.31")
                 implementation(project(":domain"))
             }
         }
