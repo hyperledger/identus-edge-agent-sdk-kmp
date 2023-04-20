901ef679f07027b202b75565436e95175877c5e5
Ahmed Moussa <ahmed.moussa@iohk.io>
2023-04-20 10:44:03 +0200

    delete no longer needed folder

--- mercury/src/allButJSTest/kotlin/io.iohk.atala.prism.walletsdk.mercury/ApiMock.kt
+++
@@ -1,7 +0,0 @@
-package io.iohk.atala.prism.walletsdk.mercury
-
-class ApiMock : Api {
-    override fun request(httpMethod: String, url: String, body: Any): ByteArray? {
-        TODO("Not yet implemented")
-    }
-}
