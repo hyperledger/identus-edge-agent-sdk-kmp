023f285f5b75ac0305db8e8b3a9e8f6f11f5d6d2
Cristian G <113917899+cristianIOHK@users.noreply.github.com>
2023-08-07 12:10:15 -0400

    fix: pr change request ATL-4965 (#90)

--- atala-prism-sdk/src/commonMain/kotlin/io/iohk/atala/prism/walletsdk/domain/models/JWTPayload.kt
+++ atala-prism-sdk/src/commonMain/kotlin/io/iohk/atala/prism/walletsdk/domain/models/JWTPayload.kt
@@ -75,60 +75,6 @@ data class JWTPayload @JvmOverloads constructor(
             return result
         }
     }
-
-    companion object {
-
-//        @JvmStatic
-//        fun fromJson(json: JsonString): JWTPayload {
-//            val jsonObject = Json.decodeFromString<JsonObject>(json)
-//
-//            val jsonVc = jsonObject.getCredentialField<JsonObject>(VC)
-//
-//            val iss = DID(jsonObject.getCredentialField(name = ISS))
-//            val sub: String? = jsonObject.getCredentialField(name = SUB, isOptional = true)
-//            val nbf: String = jsonObject.getCredentialField(name = NBF)
-//            val exp: String? = jsonObject.getCredentialField(name = EXP, isOptional = true)
-//            val jti: String = jsonObject.getCredentialField(name = JTI)
-//
-//            if (jsonObject.getCredentialField<String>(CREDENTIAL_TYPE) != CredentialType.JWT.type) {
-//                throw PolluxError.InvalidCredentialError()
-//            }
-//
-//            val vcCredentialSubject: JsonObject = jsonVc.getCredentialField(name = CREDENTIAL_SUBJECT)
-//            val vcProof: JsonObject? = jsonVc.getCredentialField(name = PROOF, isOptional = true)
-//            val credentialSubject = Json.encodeToString(vcCredentialSubject)
-//            val proof = vcProof?.let { Json.encodeToString(it) }
-//
-//            val verifiableCredential = JWTVerifiableCredential(
-//                id = jsonVc.getCredentialField(name = ID),
-//                credentialType = CredentialType.JWT,
-//                context = jsonVc.getCredentialField(name = CONTEXT),
-//                type = jsonVc.getCredentialField(name = TYPE),
-//                credentialSchema = jsonVc.getCredentialField(name = CREDENTIAL_SCHEMA, isOptional = true),
-//                credentialSubject = credentialSubject,
-//                credentialStatus = jsonVc.getCredentialField(name = CREDENTIAL_STATUS, isOptional = true),
-//                refreshService = jsonVc.getCredentialField(name = REFRESH_SERVICE, isOptional = true),
-//                evidence = jsonVc.getCredentialField(name = EVIDENCE, isOptional = true),
-//                termsOfUse = jsonVc.getCredentialField(name = TERMS_OF_USE, isOptional = true),
-//                issuer = jsonVc.getCredentialField(name = ISSUER),
-//                issuanceDate = jsonVc.getCredentialField(name = ISSUANCE_DATE),
-//                expirationDate = jsonVc.getCredentialField(name = EXPIRATION_DATE, isOptional = true),
-//                validFrom = jsonVc.getCredentialField(name = VALID_FROM, isOptional = true),
-//                validUntil = jsonVc.getCredentialField(name = VALID_UNTIL, isOptional = true),
-//                proof = proof,
-//                aud = jsonVc.getCredentialField(name = AUD),
-//            )
-//
-//            return JWTPayload(
-//                iss = iss,
-//                verifiableCredential = verifiableCredential,
-//                nbf = nbf,
-//                exp = exp,
-//                jti = jti,
-//                sub = sub
-//            )
-//        }
-    }
 }
 
 inline fun <reified T> JsonObject.getCredentialField(name: String, isOptional: Boolean = false): T {
