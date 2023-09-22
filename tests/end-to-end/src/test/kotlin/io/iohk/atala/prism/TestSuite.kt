78d0bedb8b8be026cb6ae21d39fa567a57155552
Allain Magyar <allain.magyar@iohk.io>
2023-09-21 22:01:01 -0300

    ci: add e2e execution when merge to main

--- tests/end-to-end/src/test/kotlin/io/iohk/atala/prism/TestSuite.kt
+++ tests/end-to-end/src/test/kotlin/io/iohk/atala/prism/TestSuite.kt
@@ -1,9 +1,9 @@
 package io.iohk.atala.prism
 
+import io.cucumber.java.BeforeAll
 import io.cucumber.junit.CucumberOptions
 import io.iohk.atala.prism.configuration.Environment
 import net.serenitybdd.cucumber.CucumberWithSerenity
-import org.junit.BeforeClass
 import org.junit.runner.RunWith
 
 @RunWith(CucumberWithSerenity::class)
@@ -11,13 +11,10 @@ import org.junit.runner.RunWith
     features = ["src/test/resources/features"],
     plugin = ["pretty"]
 )
-class TestSuite {
-    companion object {
-        @JvmStatic
-        @BeforeClass
-        fun setupEnvironment() {
-            Environment.setup()
-        }
-    }
-}
+class TestSuite
 
+// https://cucumber.io/docs/cucumber/api/?lang=kotlin
+@BeforeAll
+fun setupEnvironment() {
+    Environment.setup()
+}
