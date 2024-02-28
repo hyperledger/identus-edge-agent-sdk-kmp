be6734a9df93b07b01c1485b38b9e67a49b40218
Allain Magyar <allain.magyar@iohk.io>
2024-02-28 14:06:45 -0300

    test: updates client and fixes endpoint (#129)
    Co-authored-by: Ahmed Moussa <ahmed.moussa@iohk.io>
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- tests/end-to-end/build.gradle.kts
+++ tests/end-to-end/build.gradle.kts
@@ -29,8 +29,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("io.iohk.atala.prism.walletsdk:atala-prism-sdk:2.6.0")
-    testImplementation("io.iohk.atala.prism:prism-kotlin-client:1.19.1")
+    testImplementation("io.iohk.atala.prism.walletsdk:atala-prism-sdk:2.7.0")
+    testImplementation("io.iohk.atala.prism:prism-kotlin-client:1.28.0")
     testImplementation("io.iohk.atala:atala-automation:0.3.0")
 }
 
