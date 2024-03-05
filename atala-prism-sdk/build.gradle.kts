e8295eb1cb6b7cdacdfcfac4734035c96bd4a6b2
Ahmed Moussa <ahmed.moussa@iohk.io>
2024-03-05 15:46:27 +0200

    ci: fix for publishing to maven central
    Signed-off-by: Ahmed Moussa <ahmed.moussa@iohk.io>

--- atala-prism-sdk/build.gradle.kts
+++ atala-prism-sdk/build.gradle.kts
@@ -38,6 +38,16 @@ koverReport {
     }
 }
 
+/**
+ * The `javadocJar` variable is used to register a `Jar` task to generate a Javadoc JAR file.
+ * The Javadoc JAR file is created with the classifier "javadoc" and it includes the HTML documentation generated
+ * by the `dokkaHtml` task.
+ */
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaHtml)
+}
+
 kotlin {
     androidTarget {
         publishAllLibraryVariants()
@@ -51,6 +61,13 @@ kotlin {
         }
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
+        }
+        publishing {
+            publications {
+                withType<MavenPublication> {
+                    artifact(javadocJar)
+                }
+            }
         }
     }
     applyDefaultHierarchyTemplate()
