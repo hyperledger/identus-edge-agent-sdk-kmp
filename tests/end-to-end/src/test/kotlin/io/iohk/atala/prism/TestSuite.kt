5d2f9487436ef5a0370a2f993c59a0f3d9dde7d5
Allain Magyar <allain.magyar@iohk.io>
2024-04-25 14:27:15 -0300

    test: disable zkp presentation flow e2e

--- tests/end-to-end/src/test/kotlin/io/iohk/atala/prism/TestSuite.kt
+++ tests/end-to-end/src/test/kotlin/io/iohk/atala/prism/TestSuite.kt
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith
 @RunWith(CucumberWithSerenity::class)
 @CucumberOptions(
     features = ["src/test/resources/features"],
-    plugin = ["pretty"]
+    plugin = ["pretty"],
+    tags = "not (@proof and @anoncred)"
 )
 class TestSuite
