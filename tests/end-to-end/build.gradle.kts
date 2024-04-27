27f4fecd72f7b3d7c01780f604cd8f57064872f6
Allain Magyar <allain.magyar@iohk.io>
2024-04-27 11:31:44 -0300

    fix: sdk version in e2e

--- tests/end-to-end/build.gradle.kts
+++ tests/end-to-end/build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("io.iohk.atala.prism.walletsdk:atala-prism-sdk:3.0.0-K")
+    testImplementation("io.iohk.atala.prism.walletsdk:atala-prism-sdk:3.0.0")
     testImplementation("io.iohk.atala.prism:prism-kotlin-client:1.31.0")
     testImplementation("io.iohk.atala:atala-automation:0.3.2")
 }
